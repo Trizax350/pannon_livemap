8100f1127645675194b2a22201b03ddf41438f5a
Hajnal Róbert Dávid <trizax350@gmail.com>
2022-09-28 09:36:20 +0200

    conn problem fix7

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   //apiRoot: 'https://resources.eu.ngrok.io',
   apiRoot: 'http://10.77.2.14:8150',
 
-  API_URL: 'http://10.77.1.12',
+  API_URL: 'http://10.77.2.12',
   API_PORT: 4321,
   USING_PROXY: false,
   PROXY_URL: 'https://demo.alarms.hu/proxy/',
